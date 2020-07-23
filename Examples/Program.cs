9ea9355c2d092576215479bc298ad61381b71987
onrtsdmr <onurtaademir@gmail.com>
2020-07-23 14:54:48 +0300

    delegates added.

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -3,11 +3,15 @@ using Examples.Extension;
 
 namespace Examples
 {
+    delegate void MyDelegate(int numberOne, int numberTwo);
     class Program
     {
         static void Main(string[] args)
         {
             // "Hello World!".Print(); Extension Method
+            var dDelegate = new MyDelegate(Sum);
+            dDelegate.Invoke(2,3);
         }
+        public static void Sum(int numberOne, int numberTwo) => Console.WriteLine(numberOne + numberTwo);
     }
 }
